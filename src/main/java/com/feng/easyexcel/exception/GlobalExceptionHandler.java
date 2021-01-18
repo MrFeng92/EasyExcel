238ef47bf41887ec1d233395fdac66442f00e5c5
MrFeng <1406840277@qq.com>
2021-01-18 21:49:42 +0800

    异常处理

---
+++ src/main/java/com/feng/easyexcel/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.feng.easyexcel.exception;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler
+{
+
+
+//    public R error(Exception e){
+//
+//    }
+}
