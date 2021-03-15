7529581b515c87be1d77515f9f6c84b09c3cb701
MrFeng <1406840277@qq.com>
2021-03-15 20:57:46 +0800

    python

--- src/main/temp/db.py
+++ src/main/temp/db.py
@@ -3,6 +3,23 @@ import pymysql
 """
 常用模块读写MySQL数据库
 """
+import json
+
+# dumps可以格式化所有的基本数据类型为字符串
+data1 = json.dumps([])         # 列表
+print(data1, type(data1))
+data2 = json.dumps(2)          # 数字
+print(data2, type(data2))
+data3 = json.dumps('3')        # 字符串
+print(data3, type(data3))
+dict = {"name": "Tom", "age": 23}   # 字典
+data4 = json.dumps(dict)
+print(data4, type(data4))
+
+with open("test.json", "w", encoding='utf-8') as f:
+    # indent 超级好用，格式化保存字典，默认为None，小于0为零个空格
+    f.write(json.dumps(dict, indent=4))
+    json.dump(dict, f, indent=4)  # 传入文件描述符，和dumps一样的结果
 
 
 def get_conn():
